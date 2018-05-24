9bb4a05130aef6a4b5754a358f2d669f6f4f8a76
Tomas Petricek <tomas@tomasp.net>
2018-05-24 01:30:27 +0100

    Creating minimal server

---
+++ src/app.fsx
@@ -0,0 +1,30 @@
+#if INTERACTIVE
+#r "System.Xml.Linq.dll"
+open Suave.Classic.Suave.Http
+#r "../packages/Suave/lib/net40/Suave.dll"
+#else
+module Server
+#endif
+open Suave
+open System
+open Suave.Filters
+open Suave.Writers
+open Suave.Operators
+
+// When we get POST request to /log, write the received 
+// data to the log blob (on a single line)
+let app =
+  Successful.OK "Yo"
+
+// When port was specified, we start the app (in Azure), 
+// otherwise we do nothing (it is hosted by 'build.fsx')
+match System.Environment.GetCommandLineArgs() |> Seq.tryPick (fun s ->
+    if s.StartsWith("port=") then Some(int(s.Substring("port=".Length)))
+    else None ) with
+| Some port ->
+    let serverConfig =
+      { Web.defaultConfig with
+          logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Warn
+          bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" port ] }
+    Web.startWebServer serverConfig app
+| _ -> ()
